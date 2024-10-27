4ebe401929cc6a4e8d601c885703f8197d095c8c
Sadeem <Sadeem6ivnex@gmail.com>
2024-10-27 15:48:26 +0300

    Homepage

---
+++ Food recipes/Food recipes/Edit recipes .swift
@@ -0,0 +1,8 @@
+//
+//  Edit recipes .swift
+//  Food recipes
+//
+//  Created by Sadeem Orwoq on 24/04/1446 AH.
+//
+
+import Foundation
---
+++ Food recipes/Food recipes/Food_recipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_recipesApp.swift
+//  Food recipes
+//
+//  Created by Sadeem Orwoq on 20/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Food_recipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
