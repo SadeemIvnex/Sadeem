b286e1fff08625397fb662757d9a5bb69116e482
Sadeem <Sadeem6ivnex@gmail.com>
2024-10-29 14:19:09 +0300

    Home page done

---
+++ Food recipes/Food recipes/edit recipe.swift
@@ -0,0 +1,18 @@
+//
+//  edit recipe.swift
+//  Food recipes
+//
+//  Created by Sadeem Orwoq on 26/04/1446 AH.
+//
+
+import SwiftUI
+
+struct edit_recipe: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    edit_recipe()
+}
